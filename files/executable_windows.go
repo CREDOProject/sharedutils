990120ff400f33e4d95fdfbe2f1723a56e83e0d9
Eliseo Martelli <me@eliseomartelli.it>
2024-02-28 17:12:25 +0100

    fix: change case of func

--- files/executable_windows.go
+++ files/executable_windows.go
@@ -10,6 +10,6 @@ import (
 
 // isExecutable returns true if the given file info is executable.
 // On Windows, it just checks if the file extension is ".exe" or not.
-func isExecutable(info fs.FileInfo) bool {
+func IsExecutable(info fs.FileInfo) bool {
 	return strings.ToLower(filepath.Ext(info.Name())) == ".exe"
 }
